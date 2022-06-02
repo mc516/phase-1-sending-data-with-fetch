471f97552a66a7c556c8ff7b11d6ac68682cd823
Mickey <mickeyc11111@gmail.com>
2022-06-01 20:20:03 -0400

    commit tree 0d3010e73bdd3c0fd55d90481bfbacb76d146435

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    const userObj = {
+        name : userName,
+        email: userEmail,
+    };
+    const configureObj = {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(userObj),
+    };
+
+    return fetch('http://localhost:3000/users', configureObj)
+    .then(res => res.json())
+    .then(data => {
+        let domObj = document.createElement('li');
+        domObj.innerHTML = `<h1>${data.id}</h1>`;
+        document.body.appendChild(domObj);
+    })
+    .catch(error => {
+        alert("Uh Oh!");
+        let errorMessage = document.createElement('li')
+        errorMessage.innerHTML = `<h1>${error.message}</h1>`;
+        document.body.appendChild(errorMessage);
+    })
+}
+
